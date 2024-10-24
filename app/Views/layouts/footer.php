b10d2030cf2414b6742cf04adbaa162364660961
BelenSantiagoMartinez <belenmartz423@gmail.com>
2024-10-24 11:49:51 -0600

    masterpage
    Página maestra (layout)

---
+++ app/Views/layouts/footer.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo $this->renderSection("contenido"); ?>
+<footer>
+        <div class="container-fluid fcontainer p-2">
+            <div class="row text-center p-2">
+                <div class = "col-md-3">
+                    <a href="https://www.oaxaca.gob.mx/" target="_self"><img src="css/Animation - 1727669782703.gif" class="img-icon p-0 m-0 me-3"></a>
+                </div>
+                <div class = "col-md-6">
+                    <div class="row">
+                        <div class="col-12">
+                            <p style="font-family: 'Montserrat-Regular';"><small>Ciudad Administrativa Benemérito de las Américas Edificio 2 «Rufino Tamayo» Planta Baja</br>Carretera Oaxaca-Istmo Km. 11.5 Tlalixtac de Cabrera, Oaxaca C.P. 68270.</small></p>
+                        </div>
+                        <div class="col-12">
+                            <a href="https://twitter.com/honestidadoax?lang=es" target="_blank"><button type="button" class=" btn-iconrs rounded-circle"><i class="fa-brands fa-twitter"></i></button></a>
+                            <a href="https://www.facebook.com/HonestidadGobOax" target="_blank"><button type="button" class=" btn-iconrs rounded-circle"><i class="fa-brands fa-facebook-f"></i></button></a>
+                            <a href="https://www.oaxaca.gob.mx/honestidad/contacto/" target="_blank"><button type="button" class=" btn-iconrs rounded-circle"><i class="fa-regular fa-envelope"></i></button></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 logowhite-container">
+                    <a href="https://www.oaxaca.gob.mx/" target="_blank"><img src="css/ImageOK.png" class="img-iconwhite" alt="Gobierno del Estado de Oaxaca"></a>
+                    <div class="textwhite-container">
+                        <a href="https://www.oaxaca.gob.mx/" target="_blank" class="no-underline oaxaca-text">OAXACA</a>
+                        <a href="https://www.oaxaca.gob.mx/" target="_blank" class="no-underline gobierno-text">GOBIERNO DEL ESTADO</a>
+                    </div>
+                </div>            
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
